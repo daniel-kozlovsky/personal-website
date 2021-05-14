8da9b9e3b64160ca7c784f20fe704927fe695a1c
Daniel Kozlovsky <daniel.kozlovsky@gmail.com>
2021-05-14 15:14:23 -0400

    virtual showroom blurb

--- my-website/src/components/routes/Projects.jsx
+++ my-website/src/components/routes/Projects.jsx
@@ -12,7 +12,25 @@ class Projects extends React.Component {
                 </video>
                 <div className={classes.description}>
                     <h3>Virtual Showroom</h3>
-                    <p>This project was a VR showroom...</p>
+                    <p>
+                        As my capstone school project my team and I set out to solve real world problems through software engineering through a VR showroom application. 
+                        We had the chance to work with Kablan Developments Inc. to showcase their tiles in a realistic and convenient way, to save the time and effort of 
+                        physically browsing their myriad of tile designs and styles. With so many tile designs, it’s impossible to hold them all in a showroom, and is even 
+                        more tedious to transport them to construction sites showcase.
+                    </p>
+                    <p>
+                        To achieve this objective, we developed a virtual reality application through the Unity game engine that places the player in a home setting and allows 
+                        them to switch out wall and floor textures with tiles for comparison. To map controls, camera and interactions reliably, we used SteamVR. Then, graphical 
+                        fidelity was a key objective for us and prompted use of 4K textures and comprehensive post-processing. In addition, our team wanted Kablan to be able to 
+                        manage their own tile textures, since new tiles be created or removed from their lineup. To do this, we used substance alchemist in a standalone application 
+                        that processes textures to make them fit the showroom post effects. 
+                    </p>
+                    <p>
+                        While this project taught me Unity and graphical programming, the real value of the experience came from the process. I took the role of team leader and project 
+                        manager which entailed work planning, task management, and client interfacing. I became really comfortable with the SDLC and the key documentation for each phase, 
+                        helping me improve me design and planning skills. As leader I learned to guide the team and make executive decisions, consequently being accountable for deliverables 
+                        and results. It really made me feel like an owner of the application that I could indeed be proud of!
+                    </p>
                 </div>
                 <img className={classes.media} alt="pr2"/>
                 <div className={classes.description}>
